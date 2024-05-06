ae4239878a7ff7bd4dc8c611d5a630d06fd66c0e
Benoit <benoit.guibert@free.fr>
2024-05-06 10:41:11 +0200

    Changes for use as an API

--- vcf2seq/__init__.py
+++ vcf2seq/__init__.py
@@ -5,5 +5,5 @@ import sys, os
 app_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, app_path)
 
-from vcf2seq.vcf2seq import compute, vcf_ctrl
+from vcf2seq.vcf2seq import compute, input_ctrl
 
